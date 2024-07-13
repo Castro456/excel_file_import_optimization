0a92a55e3456708cdaf26f11fae21a15a3e2aff9
Castro456 <castrosid456@gmail.com>
2024-07-13 19:21:49 +0530

    removed unwanted fillable in sales model

--- app/Models/Sales.php
+++ app/Models/Sales.php
@@ -17,14 +17,5 @@ class Sales extends Model
         'item_type',
         'sales_channel',
         'order_priority',
-        'order_date',
-        'order_id',
-        'ship_date',
-        'units_sold',
-        'unit_price',
-        'unit_cost',
-        'total_revenue',
-        'total_cost',
-        'total_profit'
     ];
 }
